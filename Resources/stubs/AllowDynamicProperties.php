cbc17eea85c3a8b30b88798cf79589472dcd179d
Chris Haas <chris@vendiadvertising.com>
2021-12-01 15:31:11 -0600

    Update AllowDynamicProperties.php

--- Resources/stubs/AllowDynamicProperties.php
+++ Resources/stubs/AllowDynamicProperties.php
@@ -1,7 +1,7 @@
 <?php
 
 if (\PHP_VERSION_ID < 80200) {
-    #[Attribute(Attribute::TARGET_METHOD)]
+    #[Attribute(Attribute::TARGET_CLASS)]
     final class AllowDynamicProperties
     {
         public function __construct()
